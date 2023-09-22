4bac9130414abada46a4e84b89507dd3b37a6306
Pavel Novik <brn2befree@gmail.com>
2023-09-22 17:02:54 +0300

    start way

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <span>it-kamasutra.com footer</span>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={"header"}>
+            <img
+                src={'https://images.vexels.com/media/users/3/224169/isolated/lists/dbfe1f493ad01117fa4ec5ba10150e4d-computer-programming-logo.png'}/>
+        </header>
+    );
+};
+
+export default Header;
