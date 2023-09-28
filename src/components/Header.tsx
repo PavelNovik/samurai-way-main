70e1d6b34643a11ad89ddd5f1a7f977f04fcd019
Pavel Novik <brn2befree@gmail.com>
2023-09-28 16:44:08 +0300

    css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={"header"}>
+        <header className={classes.header}>
             <img
                 src={'https://images.vexels.com/media/users/3/224169/isolated/lists/dbfe1f493ad01117fa4ec5ba10150e4d-computer-programming-logo.png'}/>
         </header>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import classes from './Profile.module.css';
 const Profile = () => {
     return (
-        <div className={"content"}>
+        <div className={classes.content}>
             <div>
                 <img
                     src={"https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"}/>
