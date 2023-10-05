7da58f2dbf545757eed15b9be29bc3ae1a697405
Pavel Novik <brn2befree@gmail.com>
2023-10-05 09:34:39 +0300

    add grid area to dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Navbar/>
             {/*<Profile/>*/}
             <Dialogs/>
+
         </div>
     );
 }
