4b82b064d6c6d9007c3d757381bb8fea9dade704
Pavel Novik <brn2befree@gmail.com>
2023-10-29 00:27:05 +0300

    samurai way 29 add some style for nav bar and dialogs

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -5,7 +5,7 @@ const ProfileInfo = () => {
     return (
         <div className={s.profileInfo}>
             <div>
-                <img
+                <img alt={'wallpaper'}
                     src={"https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"}/>
             </div>
             <div className={s.profileInfoDescription}>
