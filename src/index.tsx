ad4c8beacef416e570e14ee23999fe08f1c67543
Pavel Novik <brn2befree@gmail.com>
2023-11-13 15:45:10 +0300

    Lesson 44 context & Provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,18 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App";
 import {StateType} from "./redux/store";
 import store from "./redux/redux-store";
-import StoreContext from "./redux/StoreContext";
+import StoreContext, {Provider} from "./redux/StoreContext";
 
 const rerenderEntireTree = (state: StateType) => {
     ReactDOM.render(
         <BrowserRouter>
-            <StoreContext.Provider value={store}>
+            {/*<StoreContext.Provider value={store}>*/}
+            <Provider store={store}>
                 <App
                     // state={state} store={store} dispatch={store.dispatch.bind(store)}
                 />
-            </StoreContext.Provider>
+            </Provider>
+            {/*</StoreContext.Provider>*/}
         </BrowserRouter>,
         document.getElementById('root')
     );
--- src/redux/StoreContext.tsx
+++ src/redux/StoreContext.tsx
@@ -6,9 +6,13 @@ const StoreContext = React.createContext<StoreType>(store);
 
 export default StoreContext;
 
+export type ProviderType = {
+    store: StoreType
+    children: React.ReactNode
+}
 
-// export const Provider = (props) => {
-//     return <StoreContext.Provider value={props.store}>
-//         {props.children}
-//     </StoreContext.Provider>
-// }
+export const Provider = (props: ProviderType) => {
+    return <StoreContext.Provider value={props.store}>
+        {props.children}
+    </StoreContext.Provider>
+}
