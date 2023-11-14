6132d5599a0ecd927661f3724a357a60849eea36
Pavel Novik <brn2befree@gmail.com>
2023-11-14 08:07:33 +0300

    Lesson 45 react-redux & connect refactored code

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import styles from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-
-// import {StoreType} from "../../redux/store";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
-type ProfilePropsType = {
-    // state: ProfilePageType
-    // dispatch: (action: StoreActionType) => void
-    // store: StoreType
-}
-const Profile = (props: ProfilePropsType) => {
+const Profile = () => {
     return (
         <div className={styles.content}>
             <ProfileInfo/>
-            <MyPostsContainer
-                // store={props.store}
-            />
+            <MyPostsContainer/>
         </div>
     );
 };
