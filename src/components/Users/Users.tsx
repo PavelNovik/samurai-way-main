a385a17d24cbb323b52f37a44db66eb067fdfa52
Pavel Novik <brn2befree@gmail.com>
2023-12-03 22:55:29 +0300

    lesson 51 add axios

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,55 +6,59 @@ import pic1 from "../../assets/img/pic1.svg";
 import pic2 from "../../assets/img/pic2.svg";
 import pic3 from "../../assets/img/pic3.jpg";
 import pic4 from "../../assets/img/pic4.jpg";
+import axios from "axios";
 
 export const Users: FC<UsersPagePropsType> = (props) => {
 
-    if (props.users.length === 0) props.setUsers([
-        {
-            id: v1(),
-            avatar: pic1,
-            isFollow: false,
-            name: 'Dmitry K.',
-            location: {
-                country: 'Belarus',
-                city: 'Minsk',
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => console.log(res.data))
+        props.setUsers([
+            {
+                id: v1(),
+                avatar: pic1,
+                isFollow: false,
+                name: 'Dmitry K.',
+                location: {
+                    country: 'Belarus',
+                    city: 'Minsk',
+                },
+                status: 'I am looking for a Job right now...'
             },
-            status: 'I am looking for a Job right now...'
-        },
-        {
-            id: v1(),
-            avatar: pic2,
-            isFollow: false,
-            name: 'Svetlana D.',
-            location: {
-                country: 'Belarus',
-                city: 'Minsk',
+            {
+                id: v1(),
+                avatar: pic2,
+                isFollow: false,
+                name: 'Svetlana D.',
+                location: {
+                    country: 'Belarus',
+                    city: 'Minsk',
+                },
+                status: 'I am so pretty'
             },
-            status: 'I am so pretty'
-        },
-        {
-            id: v1(),
-            avatar: pic3,
-            isFollow: true,
-            name: 'Sergei S.',
-            location: {
-                country: 'Ukraine',
-                city: 'Kiev',
+            {
+                id: v1(),
+                avatar: pic3,
+                isFollow: true,
+                name: 'Sergei S.',
+                location: {
+                    country: 'Ukraine',
+                    city: 'Kiev',
+                },
+                status: 'I like football!!!'
             },
-            status: 'I like football!!!'
-        },
-        {
-            id: v1(),
-            avatar: pic4,
-            isFollow: true,
-            name: 'Andrew T.',
-            location: {
-                country: 'United States',
-                city: 'Philadelphia',
-            },
-            status: 'I am free to help you to create good Video Production'
-        }
-    ])
+            {
+                id: v1(),
+                avatar: pic4,
+                isFollow: true,
+                name: 'Andrew T.',
+                location: {
+                    country: 'United States',
+                    city: 'Philadelphia',
+                },
+                status: 'I am free to help you to create good Video Production'
+            }
+        ])
+    }
 
     const users = props.users
 
